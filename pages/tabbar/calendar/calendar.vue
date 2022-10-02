9d9322f29d150ec24b579733b24f61b0a05caf7b
爸爸 <2601467204@qq.com>
2022-10-02 14:37:33 +0800

    洪丽娜构建架构

---
+++ pages/tabbar/calendar/calendar.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+	日历页面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/tabbar/completed/completed.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		已完成页面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
